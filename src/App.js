1e5b21f2addb5283b9e9e12dc8c6ce9c5fcdae8f
milan <114464208+Milan-Parmar-Me@users.noreply.github.com>
2023-03-14 15:48:28 +0530

    frist commit'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Milan from "./Components/Milan";
+
+function App()
+{
+    return (
+        <div>
+            
+            <Milan></Milan>
+        </div>
+    )
+};
+
+export default App;
---
+++ src/Components/Milan.js
@@ -0,0 +1,14 @@
+import "./Milan.css"
+
+function Milan()
+{
+    return(
+        <div className="header">
+            <div className="button">Jupiter</div>
+            <div className="button">Neptune</div>
+            <div className="button">Uranus</div>
+        </div>
+    )
+};
+
+export default Milan;
